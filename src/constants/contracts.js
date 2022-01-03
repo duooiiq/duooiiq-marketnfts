be7641230f8f8960ca13943089000895e7b7ab12
The MexToken Proyect <83620760+duooiiq@users.noreply.github.com>
2022-01-02 23:30:15 -0600

    Update contracts.js

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,5 +1,5 @@
 const addresses = {
-  NFT_CONTRACTS_ADDRESS: "0xfD2812A89Fca51D37Ef4adeB33B909a9020e4D8b",
+  NFT_CONTRACTS_ADDRESS: "0x0d06Bc636Ca2b790d0fe728ed85400bA455C1598",
 };
 export default addresses;
 
